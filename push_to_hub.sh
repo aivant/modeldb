31bd449dbc399b59d31e70c7f0ace55312add63c
Alan Menaged <alan@vant.ai>
2021-03-24 10:33:27 -0400

    Push built images to gcr rather than verta dockerhub

--- push_to_hub.sh
+++ push_to_hub.sh
@@ -13,12 +13,12 @@ fi
 
 echo "updating images with tag $tag"
 
-docker tag vertaaiofficial/modeldb-backend:latest vertaaiofficial/modeldb-backend:$tag
-docker tag vertaaiofficial/modeldb-proxy:latest vertaaiofficial/modeldb-proxy:$tag
-docker tag vertaaiofficial/modeldb-frontend:latest vertaaiofficial/modeldb-frontend:$tag
-docker tag vertaaiofficial/modeldb-graphql:latest vertaaiofficial/modeldb-graphql:$tag
+docker tag gcr.io/vantai-analysis/modeldb-backend:latest gcr.io/vantai-analysis/modeldb-backend:$tag
+docker tag gcr.io/vantai-analysis/modeldb-proxy:latest gcr.io/vantai-analysis/modeldb-proxy:$tag
+docker tag gcr.io/vantai-analysis/modeldb-frontend:latest gcr.io/vantai-analysis/modeldb-frontend:$tag
+docker tag gcr.io/vantai-analysis/modeldb-graphql:latest gcr.io/vantai-analysis/modeldb-graphql:$tag
 
-docker push vertaaiofficial/modeldb-backend:$tag
-docker push vertaaiofficial/modeldb-proxy:$tag
-docker push vertaaiofficial/modeldb-frontend:$tag
-docker push vertaaiofficial/modeldb-graphql:$tag
+docker push gcr.io/vantai-analysis/modeldb-backend:$tag
+docker push gcr.io/vantai-analysis/modeldb-proxy:$tag
+docker push gcr.io/vantai-analysis/modeldb-frontend:$tag
+docker push gcr.io/vantai-analysis/modeldb-graphql:$tag
